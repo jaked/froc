1980b71719c0e779c942e2dfe3edd1ad743fff95
Jake Donham <jake@donham.org>
2010-04-06 11:43:17 -0400

    cleanup

--- src/froc/froc_ddg.ml
+++ src/froc/froc_ddg.ml
@@ -27,6 +27,9 @@ let set_debug f =
   debug := f;
   TS.set_debug f
 
+let handle_exn = ref raise
+let set_exn_handler h = handle_exn := h
+
 type 'a result = Value of 'a | Fail of exn
 
 type 'a t = {
@@ -35,6 +38,60 @@ type 'a t = {
   mutable state : 'a result;
   mutable deps : ('a result -> unit) Dlist.t;
 }
+
+let total_eq v1 v2 = try compare v1 v2 = 0 with _ -> false
+
+exception Unset
+
+let next_id =
+  let next_id = ref 1 in
+  fun () -> let id = !next_id in incr next_id; id
+
+let make
+    ?(eq = total_eq)
+    ?(result = Fail Unset)
+    () = {
+  id = next_id ();
+  eq = eq;
+  state = result;
+  deps = Dlist.empty ();
+}
+
+let return ?eq v = make ?eq ~result:(Value v) ()
+let fail e = make ~result:(Fail e) ()
+
+let write_result t r =
+  let eq =
+    match t.state, r with
+      | Value v1, Value v2 -> t.eq v1 v2
+      | Fail e1, Fail e2 -> e1 == e2 (* XXX ? *)
+      | _ -> false in
+  if not eq
+  then begin
+    t.state <- r;
+    Dlist.iter (fun f -> try f r with e -> !handle_exn e) t.deps
+  end
+
+let write t v = write_result t (Value v)
+let write_exn t e = write_result t (Fail e)
+
+let read_result t = t.state
+
+let read t =
+  match t.state with
+    | Value v -> v
+    | Fail e -> raise e
+
+let add_dep ts t dep =
+  let dl = Dlist.add_after t.deps dep in
+  let cancel () = Dlist.remove dl in
+  TS.add_cleanup ts cancel
+
+let notify t f =
+  add_dep (TS.tick ()) t f
+
+let cleanup f =
+  TS.add_cleanup (TS.tick ()) f
 
 type reader = {
   read : unit -> unit;
@@ -87,57 +144,6 @@ let init () =
   TS.init ();
   pq := PQ.empty
 
-let next_id =
-  let next_id = ref 1 in
-  fun () -> let id = !next_id in incr next_id; id
-
-let total_eq v1 v2 = try compare v1 v2 = 0 with _ -> false
-
-exception Unset
-
-let make
-    ?(eq = total_eq)
-    ?(result = Fail Unset)
-    () = {
-  id = next_id ();
-  eq = eq;
-  state = result;
-  deps = Dlist.empty ();
-}
-
-let return ?eq v = make ?eq ~result:(Value v) ()
-let fail e = make ~result:(Fail e) ()
-
-let handle_exn = ref raise
-let set_exn_handler h = handle_exn := h
-
-let write_result t r =
-  let eq =
-    match t.state, r with
-      | Value v1, Value v2 -> t.eq v1 v2
-      | Fail e1, Fail e2 -> e1 == e2 (* XXX ? *)
-      | _ -> false in
-  if not eq
-  then begin
-    t.state <- r;
-    Dlist.iter (fun f -> try f r with e -> !handle_exn e) t.deps
-  end
-
-let write t v = write_result t (Value v)
-let write_exn t e = write_result t (Fail e)
-
-let read_result t = t.state
-
-let read t =
-  match t.state with
-    | Value v -> v
-    | Fail e -> raise e
-
-let add_dep ts t dep =
-  let dl = Dlist.add_after t.deps dep in
-  let cancel () = Dlist.remove dl in
-  TS.add_cleanup ts cancel
-
 let enqueue e _ = pq := PQ.add e !pq
 
 let add_reader t read =
@@ -146,18 +152,12 @@ let add_reader t read =
   let r = { read = read; start = start; finish = TS.tick () } in
   add_dep start t (enqueue r)
 
-let notify t f =
-  add_dep (TS.tick ()) t f
-
-let cleanup f =
-  TS.add_cleanup (TS.tick ()) f
-
 let connect t t' =
   let f _ = write_result t t'.state in
   f ();
   notify t' f
 
-let never _ _ = false
+let never_eq _ _ = false
 
 let bind_gen assign ?eq f t =
   let res = make ?eq () in
@@ -167,7 +167,7 @@ let bind_gen assign ?eq f t =
       | Value v -> try assign res (f v) with e -> write_exn res e);
   res
 
-let bind t f = bind_gen connect ~eq:never f t
+let bind t f = bind_gen connect ~eq:never_eq f t
 let (>>=) = bind
 let lift ?eq f = bind_gen write ?eq f
 let blift t ?eq f = lift ?eq f t
@@ -180,7 +180,7 @@ let try_bind_gen assign f ?eq succ err =
     with e -> write_exn res e);
   res
 
-let try_bind f succ err = try_bind_gen connect f ~eq:never succ err
+let try_bind f succ err = try_bind_gen connect f ~eq:never_eq succ err
 let try_bind_lift f ?eq succ err = try_bind_gen write f ?eq succ err
 
 let catch_gen assign f ?eq err =
@@ -192,10 +192,10 @@ let catch_gen assign f ?eq err =
       | Fail e -> try assign res (err e) with e -> write_exn res e);
   res
 
-let catch f err = catch_gen connect f ~eq:never err
+let catch f err = catch_gen connect f ~eq:never_eq err
 let catch_lift f ?eq err = catch_gen write f ?eq err
 
-let finish = ref []
+let finish = Stack.create ()
 
 let rec prop ?until () =
   if not (PQ.is_empty !pq)
@@ -209,9 +209,9 @@ let rec prop ?until () =
         | _ ->
             pq := PQ.remove_min !pq;
             TS.set_now r.start;
-            finish := r.finish :: !finish;
+            Stack.push r.finish finish;
             r.read ();
-            finish := List.tl !finish;
+            ignore (Stack.pop finish);
             TS.splice_out (TS.get_now ()) r.finish;
             prop ()
 
@@ -231,10 +231,10 @@ let memo ?size ?hash ?eq () =
   fun f k ->
     let result =
       try
-        if !finish = [] then raise Not_found;
+        if Stack.is_empty finish then raise Not_found;
         let ok m =
           TS.compare (TS.get_now ()) m.m_start = -1 &&
-          TS.compare m.m_finish (List.hd !finish) = -1 in
+          TS.compare m.m_finish (Stack.top finish) = -1 in
         let m = Froc_hashtbl.find h k ok in
         TS.splice_out (TS.get_now ()) m.m_start;
         prop ~until:m.m_finish ();
@@ -268,7 +268,7 @@ let bind2_gen assign ?eq f t1 t2 =
   add_dep start t2 (enqueue r);
   res
 
-let bind2 t1 t2 f = bind2_gen connect ~eq:never f t1 t2
+let bind2 t1 t2 f = bind2_gen connect ~eq:never_eq f t1 t2
 let lift2 ?eq f = bind2_gen write ?eq f
 let blift2 t1 t2 ?eq f = lift2 ?eq f t1 t2
 
@@ -290,7 +290,7 @@ let bind3_gen assign ?eq f t1 t2 t3 =
   add_dep start t3 (enqueue r);
   res
 
-let bind3 t1 t2 t3 f = bind3_gen connect ~eq:never f t1 t2 t3
+let bind3 t1 t2 t3 f = bind3_gen connect ~eq:never_eq f t1 t2 t3
 let lift3 ?eq f = bind3_gen write ?eq f
 let blift3 t1 t2 t3 ?eq f = lift3 ?eq f t1 t2 t3
 
@@ -314,7 +314,7 @@ let bind4_gen assign ?eq f t1 t2 t3 t4 =
   add_dep start t4 (enqueue r);
   res
 
-let bind4 t1 t2 t3 t4 f = bind4_gen connect ~eq:never f t1 t2 t3 t4
+let bind4 t1 t2 t3 t4 f = bind4_gen connect ~eq:never_eq f t1 t2 t3 t4
 let lift4 ?eq f = bind4_gen write ?eq f
 let blift4 t1 t2 t3 t4 ?eq f = lift4 ?eq f t1 t2 t3 t4
 
@@ -340,7 +340,7 @@ let bind5_gen assign ?eq f t1 t2 t3 t4 t5 =
   add_dep start t5 (enqueue r);
   res
 
-let bind5 t1 t2 t3 t4 t5 f = bind5_gen connect ~eq:never f t1 t2 t3 t4 t5
+let bind5 t1 t2 t3 t4 t5 f = bind5_gen connect ~eq:never_eq f t1 t2 t3 t4 t5
 let lift5 ?eq f = bind5_gen write ?eq f
 let blift5 t1 t2 t3 t4 t5 ?eq f = lift5 ?eq f t1 t2 t3 t4 t5
 
@@ -368,7 +368,7 @@ let bind6_gen assign ?eq f t1 t2 t3 t4 t5 t6 =
   add_dep start t6 (enqueue r);
   res
 
-let bind6 t1 t2 t3 t4 t5 t6 f = bind6_gen connect ~eq:never f t1 t2 t3 t4 t5 t6
+let bind6 t1 t2 t3 t4 t5 t6 f = bind6_gen connect ~eq:never_eq f t1 t2 t3 t4 t5 t6
 let lift6 ?eq f = bind6_gen write ?eq f
 let blift6 t1 t2 t3 t4 t5 t6 ?eq f = lift6 ?eq f t1 t2 t3 t4 t5 t6
 
@@ -398,7 +398,7 @@ let bind7_gen assign ?eq f t1 t2 t3 t4 t5 t6 t7 =
   add_dep start t7 (enqueue r);
   res
 
-let bind7 t1 t2 t3 t4 t5 t6 t7 f = bind7_gen connect ~eq:never f t1 t2 t3 t4 t5 t6 t7
+let bind7 t1 t2 t3 t4 t5 t6 t7 f = bind7_gen connect ~eq:never_eq f t1 t2 t3 t4 t5 t6 t7
 let lift7 ?eq f = bind7_gen write ?eq f
 let blift7 t1 t2 t3 t4 t5 t6 t7 ?eq f = lift7 ?eq f t1 t2 t3 t4 t5 t6 t7
 
@@ -418,6 +418,6 @@ let bindN_gen assign ?eq f ts =
   List.iter (fun t -> add_dep start t (enqueue r)) ts;
   res
 
-let bindN ts f = bindN_gen connect ~eq:never f ts
+let bindN ts f = bindN_gen connect ~eq:never_eq f ts
 let liftN ?eq f = bindN_gen write ?eq f
 let bliftN ts ?eq f = liftN ?eq f ts
