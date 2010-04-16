3673fc5ceb13a658bddb2832c091d133ff0b5654
Jake Donham <jake@donham.org>
2010-04-15 21:56:05 -0400

    copyright

--- src/froc-dom/froc_dom.ml
+++ src/froc-dom/froc_dom.ml
@@ -1,6 +1,6 @@
 (*
  * This file is part of froc, a library for functional reactive programming
- * Copyright (C) 2009 Jacob Donham
+ * Copyright (C) 2009-2010 Jacob Donham
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Library General Public
